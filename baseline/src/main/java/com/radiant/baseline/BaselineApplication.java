3ddd493889c5bf754af9a1562773b12bcfcaa840
nyasimi <thenextrenaissance892@gmail.com>
2024-02-18 08:51:54 +0300

    Connecting to Xampp MySQL

---
+++ baseline/src/main/java/com/radiant/baseline/BaselineApplication.java
@@ -0,0 +1,19 @@
+package com.radiant.baseline;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaselineApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaselineApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+
+}
---
+++ baseline/src/test/java/com/radiant/baseline/BaselineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radiant.baseline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaselineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
